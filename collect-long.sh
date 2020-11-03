e926a3bf610948fc25a847faf277bbb1a3842711
Sergey Zhumatiy <serg@parallel.ru>
2020-11-03 11:25:13 +0300

    terminal added

--- collect-long.sh
+++ collect-long.sh
@@ -4,5 +4,7 @@ export CLUSTER='lomonosov-2'
 export YEAR_SRV='https://msumobile.rcc.msu.ru:88/WS/supercompyearinfo.wsdl'
 export YEAR_KEY='7JEA8V2Z9M4LKSFFE'
 
+export TERM=vt100
+
 bundle exec ruby ./slurm-collecter-long.rb "$@"
 
